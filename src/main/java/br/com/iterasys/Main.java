c0e313078770e64da36c13878cc4decfa4e05350
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-06-01 14:55:34 -0300

    Desenvolvimento do projeto

--- src/main/java/br/com/iterasys/Main.java
+++ src/main/java/br/com/iterasys/Main.java
@@ -7,11 +7,14 @@ public class Main {
         Calculadora.somarInteiros(5, 7);
         Calculadora.subtrairInteiros(8, 4);
         Calculadora.multiplicarInteiros(3, 8);
-        Calculadora.dividirInteiros(28, 2);
+        chamarDividirInteiros();
     }
 
     public static void chamarEncomenda(){
         int barras = 30;
         Encomenda.calcularBarrasDeChocolatePorCaixa(barras);
+    }
+    public static void chamarDividirInteiros(){
+        Calculadora.dividirInteiros(28, 2);
     }
 }
