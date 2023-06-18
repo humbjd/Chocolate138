69724ae0655f01f34181441beb00d596d0b458e6
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-06-18 20:40:56 -0300

    Teste de API - testAuthorized - testResearchUserNotAuthorized - testResearchUser

--- src/test/java/apiTest/Account.java
+++ src/test/java/apiTest/Account.java
@@ -24,14 +24,14 @@ public class Account {
 
     // Instanciar Classes Externas
     Gson gson = new Gson(); // Instancia o objeto de conversão classe para json
-
+    AccountEntity account = new AccountEntity(); // Instancia a entidade usuário
     // Metodos e Funcoes
     // Metodo #1 - Criar Usuário
     @Test(priority = 1)
     public void testCreateUser(){
         // Configura - Arrange
-        AccountEntity account = new AccountEntity(); // Instancia a entidade usuário
-        account.userName = "charlie309"; // entrada e saida (resultado esperado)
+
+        account.userName = "charlie328"; // entrada e saida (resultado esperado)
         account.password = "P@ss0rd1"; // entrada
 
         jsonBody = gson.toJson(account); // Converte a entidade usuário no formato json
@@ -61,7 +61,7 @@ public class Account {
 
         // extrair o UserID (identificação do usuário)
          userId = resposta.jsonPath().getString("userID");
-         System.out.println("UserID extraido: " + userId);
+         System.out.println("UserId extraido: " + userId);
 
 
 
@@ -96,6 +96,87 @@ public class Account {
         Assert.assertTrue(token.length() != 0);
     }// fim do metodo de geração de token de identificação do usuario
 
+    @Test(priority = 3)
+    public void testAuthorized(){
+        // Configura
+        // Dados de Entrada
+        // --> Fornecidos pela AccountEntity através do metodo testCreatUser - priority 1
+
+        // Dados de Saída / Resultado Esperado
+        // StatusCode = 200
+        // Response Body = true
+
+        // Dados de Saida
+
+        // Executa
+        given()
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri + "Authorized")
+
+        // Valida
+        .then()
+                .log().all()
+                .statusCode(200)
+                //.body(true) // ToDo: como validar o retorno do body apenas como true
+        ; // fim do codigo testAuthorized
+
+    } // fim do testAuthorized
+
+
+    @Test(priority = 4)
+    public void testResearchUserNotAuthorized(){
+        // Configura
+        // Dados de Entrada
+        // userID foi extraido no metodo testCreateUser e está na memória
+        // Dados de Saída / Resultado Esperado
+        // Status Code = 401, Code = 1200 e Message = User not authorized!
+
+
+
+        // Executa
+        given()                                             // Dado // Comandos do Rest - Assured
+                .contentType(ct)                            // Formato da mensagem
+                .log().all()                                // Exibir tudo que acontece na ida
+        .when()                                             // Quando
+                .get(uri + "User/" + userId)           // Consulta o ususario pelo userId
+        // Valida
+        .then()                                             // Então
+                .log().all()                                // Exibir tudo o que acontece
+                .statusCode(401)            // Valida se nao esta autorizado
+                .body("code", is("1200"))        // Valida o codigo de mensagem nao autorizado
+                .body("message", is("User not authorized!"))
+    ;// fim do codigo testResearchUser
+    }
+
+
+    @Test(priority = 5)
+    public void testResearchUser(){
+        // Configura
+        // Dados de Entrada
+        // userID foi extraido no metodo testCreateUser e está na memória
+        // Dados de Saída / Resultado Esperado
+        // userName virá da classe Account e o status code deve ser 200
+
+
+
+        // Executa
+        given()                                             // Dado // Comandos do Rest - Assured
+                .contentType(ct)                            // Formato da mensagem
+                .log().all()                                // Exibir tudo que acontece na ida
+                .header("Authorization", "Bearer " + token)
+        .when()                                             // Quando
+                .get(uri + "User/" + userId)           // Consulta o ususario pelo userId
+                // Valida
+        .then()                                             // Então
+                .log().all()                                // Exibir tudo o que acontece
+                .statusCode(200)            // Valida se a conexao teve sucesso
+                //.body("userId", is(userId))
+                .body("username", is(account.userName)) // Valida o nome do usuário
+        ;// fim do codigo testResearchUser
+    }
 
 
 }
