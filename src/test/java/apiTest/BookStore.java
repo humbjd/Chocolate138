3b21cd8014bae797cb1fe5b8b1440d2dd71af490
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-07-04 22:28:59 -0300

    Teste de API - adicionado testUpdateLoan e testLoanBooks

--- src/test/java/apiTest/BookStore.java
+++ src/test/java/apiTest/BookStore.java
@@ -2,12 +2,11 @@ package apiTest;
 
 import com.google.gson.Gson;
 import entities.LoanEntity;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.ITestContext;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class BookStore {
 
@@ -18,14 +17,15 @@ public class BookStore {
     Gson gson = new Gson();
     LoanEntity isbn = new LoanEntity(); // Instancia a lista de livros
     public ITestContext context;
-    @BeforeMethod // Antes de cada @Teste
+    @BeforeClass // Antes da classe e de todos os testes antes dela
+    //@BeforeMethod // Antes de cada @Teste
     public void setUp(ITestContext context){
 
         account.testCreateUser(context);    // Cria um novo usuario
         account.testGerenateToken(context); // Gera um novo token
     }
-
-    @AfterMethod // Depois de cada @Teste
+    @AfterClass // Depois da classe
+    //@AfterMethod // Depois de cada @Teste
     public void tearDown(){
         account.testDeleteUser(); // excluir o usuário
 
@@ -58,8 +58,11 @@ public class BookStore {
             // Configura
             // Dados de entrada
             // userId virá pelo context
-            isbn.userId = context.getAttribute("userID").toString();
-            isbn.code[0] = "9781449325862";
+            isbn.userId = context.getAttribute("userID").toString(); // código do usuário
+            isbn.collectionOfIsbns = new LoanEntity.ISBN[]{
+                    new LoanEntity.ISBN("9781449325862")
+            };
+            //isbn.isbn = "9781449325862"; // código do livro
             // Dados de saida
             // statusCode = 201
             // Retorne o isbn do livro emprestado (echo)
@@ -71,15 +74,45 @@ public class BookStore {
                     .header("Authorization", "Bearer " + context.getAttribute("token"))
                     .body(gson.toJson(isbn))
             .when()
-                    .post(uri)
-
+                    .post(uri + "Books")
             // Valida
-
-
-
+            .then()
+                    .log().all()
+                    .statusCode(201)
+                    .body("isbn", is(isbn.isbn))
+            ;
 
         }
+        @Test(priority = 3)
+        public void testUpdateLoan(ITestContext context){ // Atualizar quem está com qual livro
+            // Configura
+            // Dados de Entrada
+            // userId é extraido do BeforeMethod
+            String isbnAntigo = "9781449325862"; // Livro a substituir
+            String isbnNovo = "9781449331818"; // Novo livro a ser emprestado
 
 
+            // alimentar a classe LoanEntity apenas com o codigo do usuario e o isbn
+            isbn = new LoanEntity(); // reiniciando o objeto da classe LoanEntity
+            isbn.userId = context.getAttribute("userId").toString(); // código do usuário
+            isbn.isbn = isbnNovo; // codigo do livro que estava com o usuario
+
+            // Executa
+            given()
+                    .log().all()
+                    .contentType(ct)
+                    .header("Authorization", "Bearer " + context.getAttribute("token"))
+                    .body(gson.toJson(isbn))
+
+            .when()
+                    .put(uri + "Books/" + isbnAntigo)
+            // Valida
+            .then()
+                    .log().all()
+                    .statusCode(200)
+                    .body("books[0].isbn", is(isbnNovo))
+            ;// fim do codigo
+
+        }
 
 }
--- src/test/java/entities/LoanEntity.java
+++ src/test/java/entities/LoanEntity.java
@@ -2,5 +2,17 @@ package entities;
 
 public class LoanEntity {
     public String userId;
-    public String[] code; // lista com os códigos dos livros emprestados
+    public ISBN[] collectionOfIsbns; // lista com os códigos dos livros emprestados
+
+    public String isbn; // código do livro
+
+    public static class ISBN{
+        private final String isbn; // codigo do livro interno
+
+        public ISBN(String isbn){          // Construtor
+            this.isbn = isbn;
+        }
+    }
+
+
 }
