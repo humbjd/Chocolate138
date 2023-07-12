b08b1cdf6b0d40ddd10e562577ce1c33ffa5ac9f
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-07-11 21:51:11 -0300

    Script do Selenium IDE com os ajustes necessarios

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,13 @@ repositories {
 dependencies {
     testImplementation("org.testng:testng:7.7.1")
     testImplementation("io.rest-assured:rest-assured:5.3.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.10.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation ("org.slf4j:slf4j-api:1.7.33")
+    testImplementation("junit:junit:4.13.1")
+    // https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager
+    implementation("io.github.bonigarcia:webdrivermanager:5.4.0")
+
 
 }
 
